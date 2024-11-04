6f6c511d4f1ad1314b24e0ac70e19cc4a00d8ab1
sinanomassery <designersinan706@gmail.com>
2024-11-04 23:48:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function addNumbers(a, b) {
+    if ( a == "number" &&  b == "number") {
+        return a + b;
+    } else {
+        console.log("Arguments must be numbers");
+    }
+   
+    
+} let result = addNumbers(5, 10);
+console.log("result");
+
+
